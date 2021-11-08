572ab514ad7925361033565eff169e41ed246b92
NaokiKamei <nkamegre4nhns9@gmail.com>
2021-11-08 16:06:27 +0000

    [機能追加]ユーザー認証

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module FoodStaff
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    
+    #以下の記述を追記する(設定必須)
+    # config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
   end
 end
+
