83252ef2ff9a5b7fba4723697a5f36bd73227c92
NaokiKamei <nkamegre4nhns9@gmail.com>
2021-11-15 11:33:50 +0000

    [追加]いいね機能非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
   
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_user.favorites.new(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = @recipe.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to recipe_path(recipe)
   end
 
   def index
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_user.favorites.find_by(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = @recipe.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to recipe_path(recipe)
   end
   
 end
