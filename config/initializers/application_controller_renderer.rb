cf4aed9efbf8a24972eff1ea2e6ae1ba6d2fe2b6
kameinaoki <nkamegre4nhns9@gmail.com>
2021-12-14 12:49:03 +0000

    update application controller renderer

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = 'e9aab27978215e3dd2789665dc7c44511e7594cbb34be39b254eb12c410b2724afe271efff
+cbe1f882251e2ae0087aa6406e497c9d2959bee643514e92b5b941'
